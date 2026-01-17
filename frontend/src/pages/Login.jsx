09b3db260b393cf7a14066952e40e47c82cf56f3
Akshargangani <akshargangani2006@gmail.com>
2026-01-17 10:01:00 +0530

    Login.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginForm from '../components/auth/LoginForm';
+
+/**
+ * Login page - User authentication page
+ */
+const Login = () => {
+  return <LoginForm />;
+};
+
+export default Login;
