5e000040d70a1fa5d8ea995cdc6e4da96bbb03d6
Akshargangani <akshargangani2006@gmail.com>
2026-01-17 10:00:23 +0530

    Cart.jsx

---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Cart from '../components/cart/Cart';
+
+/**
+ * Cart page - Shopping cart page wrapper
+ */
+const CartPage = () => {
+  return <Cart />;
+};
+
+export default CartPage;
