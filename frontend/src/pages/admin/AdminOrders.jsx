752e3662fd2d868dc3f8cfee4ab8849ee263a73c
Akshargangani <akshargangani2006@gmail.com>
2026-01-18 16:58:29 +0530

    AdminOrders.jsx

---
+++ frontend/src/pages/admin/AdminOrders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminOrders from '../../components/admin/AdminOrders';
+
+/**
+ * Admin Orders page - Order management page
+ */
+const AdminOrdersPage = () => {
+  return <AdminOrders />;
+};
+
+export default AdminOrdersPage;
