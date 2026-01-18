6634648c73628dbe58aef064dc5901c6bfe0d3c7
Akshargangani <akshargangani2006@gmail.com>
2026-01-18 16:57:31 +0530

    AdminDashboard.jsx

---
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminDashboard from '../../components/admin/AdminDashboard';
+
+/**
+ * Admin Dashboard page - Main admin dashboard page
+ */
+const AdminDashboardPage = () => {
+  return <AdminDashboard />;
+};
+
+export default AdminDashboardPage;
