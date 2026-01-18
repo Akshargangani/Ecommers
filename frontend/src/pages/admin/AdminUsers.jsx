f1302976ae4b38f9e450f36609d15178e661ce27
Akshargangani <akshargangani2006@gmail.com>
2026-01-18 16:59:27 +0530

    AdminUsers.jsx

---
+++ frontend/src/pages/admin/AdminUsers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminUsers from '../../components/admin/AdminUsers';
+
+/**
+ * Admin Users page - User management page
+ */
+const AdminUsersPage = () => {
+  return <AdminUsers />;
+};
+
+export default AdminUsersPage;
