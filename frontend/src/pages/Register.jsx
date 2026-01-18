3327b41246ac1ae3c3fff6b9b0ddbec07d0cf179
Akshargangani <akshargangani2006@gmail.com>
2026-01-18 17:00:13 +0530

    Register.jsx

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RegisterForm from '../components/auth/RegisterForm';
+
+/**
+ * Register page - User registration page
+ */
+const Register = () => {
+  return <RegisterForm />;
+};
+
+export default Register;
