7c5b95a9f66258df48fe401e96345dd499553518
Akshargangani <akshargangani2006@gmail.com>
2026-01-18 16:58:57 +0530

    AdminProducts.jsx

---
+++ frontend/src/pages/admin/AdminProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminProducts from '../../components/admin/AdminProducts';
+
+/**
+ * Admin Products page - Product management page
+ */
+const AdminProductsPage = () => {
+  return <AdminProducts />;
+};
+
+export default AdminProductsPage;
