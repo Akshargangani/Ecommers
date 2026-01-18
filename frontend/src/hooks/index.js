69c50cb8c4f60f7edfae519523472f7032d9de50
Akshargangani <akshargangani2006@gmail.com>
2026-01-18 17:30:09 +0530

    index.js

---
+++ frontend/src/hooks/index.js
@@ -0,0 +1,7 @@
+// Custom hooks for the Ecommerce application
+export { default as useLocalStorage } from './useLocalStorage';
+export { default as useDebounce } from './useDebounce';
+export { default as useApi } from './useApi';
+
+// Re-export for convenience
+export { useLocalStorage, useDebounce, useApi };
