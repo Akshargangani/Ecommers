7174ed069e333fcb1c190f81c390a6a1883a92ae
Akshargangani <akshargangani2006@gmail.com>
2026-01-21 12:21:27 +0530

    generateToken.js

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+/**
+ * Generate JWT token for user authentication
+ * @param {string} id - User ID
+ * @returns {string} JWT token
+ */
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d', // Token expires in 30 days
+  });
+};
+
+module.exports = generateToken;
